17057e73e176c104d97a4bd881db3c4296675330
Weijia Qian <ainibuni@gmail.com>
2026-01-20 16:57:20 -0500

    update simulations

--- source/main_simulations_k10.R
+++ source/main_simulations_k10.R
@@ -324,5 +324,5 @@ for (i in 1:nsim) {
 # date <- gsub("-", "", Sys.Date())
 dir.create(file.path(here("results"), k10), showWarnings = FALSE, recursive = TRUE)
 
-filename <- file.path(here("results", k10), paste0(batch, "_k10_batch1.RDA"))
+filename <- file.path(here("results", k10), paste0(batch, "_k10_batch2.RDA"))
 save(results, file = filename)

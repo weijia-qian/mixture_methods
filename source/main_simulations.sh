cdaf3ba03a225bc7c29e6e1d28309bd52f296090
Weijia Qian <ainibuni@gmail.com>
2026-01-20 15:33:25 -0500

    update simulations

--- source/main_simulations.sh
+++ source/main_simulations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-54%54
-#SBATCH --job-name=simulations_4
+#SBATCH --job-name=simulations_5
 #SBATCH --partition=week-long-cpu
-#SBATCH --output=main_simulations_4.out
-#SBATCH --error=main_simulations_4.err
+#SBATCH --output=main_simulations_5.out
+#SBATCH --error=main_simulations_5.err
 
 module purge
 module load R/4.4.0

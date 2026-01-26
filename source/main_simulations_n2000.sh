c1d061e26e47e268d05ea9ab631c64077234941c
Weijia Qian <ainibuni@gmail.com>
2026-01-26 10:22:09 -0500

    update simulations

--- source/main_simulations_n2000.sh
+++ source/main_simulations_n2000.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-6%6
-#SBATCH --job-name=n2000_2
+#SBATCH --job-name=n2000_3
 #SBATCH --partition=wrobel
-#SBATCH --output=n2000_2.out
-#SBATCH --error=n2000_2.err
+#SBATCH --output=n2000_3.out
+#SBATCH --error=n2000_3.err
 
 module purge
 module load R/4.4.0

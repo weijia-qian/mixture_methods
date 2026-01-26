ff0108ec8911e813b814429651c1ddad3a26a78b
Weijia Qian <ainibuni@gmail.com>
2026-01-25 21:26:30 -0500

    update simulations

--- source/main_simulations_k10.sh
+++ source/main_simulations_k10.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-18%18
-#SBATCH --job-name=k10_20_2
+#SBATCH --job-name=k10_20_3
 #SBATCH --partition=wrobel
-#SBATCH --output=k10_20_2.out
-#SBATCH --error=k10_20_2.err
+#SBATCH --output=k10_20_3.out
+#SBATCH --error=k10_20_3.err
 
 module purge
 module load R/4.4.0

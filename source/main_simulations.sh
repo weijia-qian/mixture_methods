903023bc04cffd81a41fd92326f0ab8866d0fc81
Weijia Qian <ainibuni@gmail.com>
2026-01-29 23:15:59 -0500

    update simulations

--- source/main_simulations.sh
+++ source/main_simulations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-9%9
-#SBATCH --job-name=inter_3
-#SBATCH --partition=wrobel
-#SBATCH --output=inter_3.out
-#SBATCH --error=inter_3.err
+#SBATCH --job-name=inter_4
+#SBATCH --partition=chang
+#SBATCH --output=inter_4.out
+#SBATCH --error=inter_4.err
 
 module purge
 module load R/4.4.0

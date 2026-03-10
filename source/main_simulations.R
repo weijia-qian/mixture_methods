b268477a8d7c88b7211c974b959cba04492f5ca9
Weijia Qian <ainibuni@gmail.com>
2026-03-10 17:40:57 -0400

    minor revision

--- source/main_simulations.R
+++ source/main_simulations.R
@@ -308,5 +308,5 @@ Date = gsub("-", "", Sys.Date())
 # filename = paste0(here::here("outputs", Date), "/", paste0(batch, "_batch1.RDA"))
 
 dir.create(file.path(here("results"), "interactive"), showWarnings = FALSE, recursive = TRUE)
-filename <- file.path(here("results", "interactive"), paste0(batch, "_batch3.RDA"))
+filename <- file.path(here("results", "interactive"), paste0(batch, "_batch4.RDA"))
 save(results, file = filename)

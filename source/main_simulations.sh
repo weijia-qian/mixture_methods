8df5a8088961a3fa9a1aa8c52a2eba184d201ca7
Weijia Qian <ainibuni@gmail.com>
2026-03-10 17:42:10 -0400

    minor revision

--- source/main_simulations.sh
+++ source/main_simulations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-18%18
-#SBATCH --job-name=inter_6
+#SBATCH --job-name=inter_7
 #SBATCH --partition=wrobel
-#SBATCH --output=inter_6.out
-#SBATCH --error=inter_6.err
+#SBATCH --output=inter_7.out
+#SBATCH --error=inter_7.err
 
 module purge
 module load R/4.4.0

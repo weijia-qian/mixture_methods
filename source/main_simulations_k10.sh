dce2c41a92cc844dd3a20e7f7a4365d940e102b8
Weijia Qian <ainibuni@gmail.com>
2026-03-10 23:33:59 -0400

    minor revision

--- source/main_simulations_k10.sh
+++ source/main_simulations_k10.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --array=1-12%12
-#SBATCH --job-name=k10_20_4
+#SBATCH --job-name=k10_20_5
 #SBATCH --partition=week-long-cpu
-#SBATCH --output=k10_20_4.out
-#SBATCH --error=k10_20_4.err
+#SBATCH --output=k10_20_5.out
+#SBATCH --error=k10_20_5.err
 
 module purge
 module load R/4.4.0

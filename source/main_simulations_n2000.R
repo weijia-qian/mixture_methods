5616fdfcefe7a182218ca490a55da42d2fa58582
Weijia Qian <ainibuni@gmail.com>
2026-03-10 23:17:30 -0400

    minor revision

--- source/main_simulations_n2000.R
+++ source/main_simulations_n2000.R
@@ -303,5 +303,5 @@ for (i in 1:nsim) {
 # date <- gsub("-", "", Sys.Date())
 dir.create(file.path(here("results"), "interactive"), showWarnings = FALSE, recursive = TRUE)
 
-filename <- file.path(here("results", "interactive"), paste0(batch, "_n2000_batch8.RDA"))
+filename <- file.path(here("results", "interactive"), paste0(batch, "_n2000_batch9.RDA"))
 save(results, file = filename)
